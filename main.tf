0a03451ec59a7116d2e3dc1c433b587bd14da128
wokia23 <wokiakumase@gmail.com>
2025-04-04 16:27:01 -0400

    added dev ser and new prod to test webhook

--- main.tf
+++ main.tf
@@ -18,3 +18,19 @@ resource "aws_instance" "airbnb-prod-server" {
     Name = "airbnb-prod-server"
   }
 }
+resource "aws_instance" "airbnb-prod-server" {
+  ami           =var.ami_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "airbnb-prod-server"
+  }
+}
+resource "aws_instance" "airbnb-dev-server" {
+  ami           =var.ami_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "airbnb-dev-server"
+  }
+}
